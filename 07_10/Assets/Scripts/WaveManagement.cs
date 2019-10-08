64012666af8ce3ff3d89e672ba46d3a9e16b5a8b
KelvinKegel <51788952+KelvinKegel@users.noreply.github.com>
2019-10-08 11:55:31 -0300

    Wave

---
+++ 07_10/Assets/Scripts/WaveManagement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManagement : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    private void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+
+    void Update()
+    {
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                //start spawn função
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i <= _wave.count; i++)
+
+        state = SpawnState.WAITING
+
+        yield break;
+    } 
+}
