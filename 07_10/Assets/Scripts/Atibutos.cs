61e5d3b75081d92df994c7a4c4bcfbfb278203f3
DinHoo <51788868+DinHoo@users.noreply.github.com>
2019-10-09 12:11:12 -0300

    aqsw

---
+++ 07_10/Assets/Scripts/Atibutos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atibutos : MonoBehaviour
+{
+
+    protected int life;
+    public int Life
+    {
+        get { return life; }
+
+        set { life = value; }
+    }
+
+       
+    
+}
---
+++ 07_10/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Atibutos
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 30;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
